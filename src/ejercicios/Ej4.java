b9caf6d6ad659309faace3c2c6a97ad3170dcc17
enieves <enieves@106-02.info2>
2023-10-04 10:13:25 +0200

    tset

--- src/ejercicios/Ej4.java
+++ src/ejercicios/Ej4.java
@@ -9,16 +9,22 @@ public class Ej4 {
 		//creación de scanner
 		Scanner sc=new Scanner(System.in);
 		
+		//variables que representan los numeros que introducimos
 		int num,num2;
 		
+		//pedimos que pobngamos un numero
 		System.out.println("Introduzca el primer numero: ");
 		
+		//el valor de ese numero se guarda en la variable correspondiente
 		num=sc.nextInt();
 		
+		//pedimos que introduzca otro
 		System.out.println("Ahora introduce otro: ");
 		
+		//el valor del otro numero se guarda en la variable que le corresponde
 		num2=sc.nextInt();
 		
+		//si uno de los numeros es mayor que el otro, se ordena de menor a mayor
 		if (num<num2) {
 			System.out.println(num+", "+num2);
 		}
